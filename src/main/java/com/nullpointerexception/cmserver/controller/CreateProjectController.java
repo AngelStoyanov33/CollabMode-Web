e482e9406fef6f4fa0acc9ad2d0c84453b89744b
Angel Stoyanov <aststoyanov@gmail.com>
2021-03-15 22:24:33 +0200

    Added create project controller

---
+++ src/main/java/com/nullpointerexception/cmserver/controller/CreateProjectController.java
@@ -0,0 +1,8 @@
+package com.nullpointerexception.cmserver.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CreateProjectController {
+
+}
